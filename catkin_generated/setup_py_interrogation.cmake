605f2221eec126d614882f298e04fc55ac7f8564
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-08-16 10:31:16 -0400

    semi build

---
+++ catkin_generated/setup_py_interrogation.cmake
@@ -0,0 +1,7 @@
+set(genjava_SETUP_PY_SETUP_MODULE "distutils.core")
+set(genjava_SETUP_PY_VERSION "0.4.0")
+set(genjava_SETUP_PY_SCRIPTS "scripts/genjava_message_artifacts")
+set(genjava_SETUP_PY_PACKAGES "genjava")
+set(genjava_SETUP_PY_PACKAGE_DIRS "src/genjava")
+set(genjava_SETUP_PY_MODULES "")
+set(genjava_SETUP_PY_MODULE_DIRS "")
