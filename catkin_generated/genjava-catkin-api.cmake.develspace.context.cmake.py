e6f6137c6b453fcd6dd75d8de988606d7604cb2d
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-08-16 10:31:16 -0400

    semi build

---
+++ catkin_generated/genjava-catkin-api.cmake.develspace.context.cmake.py
@@ -0,0 +1,33 @@
+# generated from catkin/cmake/template/cfg-extras.context.py.in
+DEVELSPACE = 'TRUE' == 'TRUE'
+INSTALLSPACE = 'FALSE' == 'TRUE'
+
+CATKIN_DEVEL_PREFIX = '/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava/devel'
+
+CATKIN_GLOBAL_BIN_DESTINATION = 'bin'
+CATKIN_GLOBAL_ETC_DESTINATION = 'etc'
+CATKIN_GLOBAL_INCLUDE_DESTINATION = 'include'
+CATKIN_GLOBAL_LIB_DESTINATION = 'lib'
+CATKIN_GLOBAL_LIBEXEC_DESTINATION = 'lib'
+CATKIN_GLOBAL_PYTHON_DESTINATION = 'lib/python3/dist-packages'
+CATKIN_GLOBAL_SHARE_DESTINATION = 'share'
+
+CATKIN_PACKAGE_BIN_DESTINATION = 'lib/genjava'
+CATKIN_PACKAGE_ETC_DESTINATION = 'etc/genjava'
+CATKIN_PACKAGE_INCLUDE_DESTINATION = 'include/genjava'
+CATKIN_PACKAGE_LIB_DESTINATION = 'lib'
+CATKIN_PACKAGE_LIBEXEC_DESTINATION = 'lib/genjava'
+CATKIN_PACKAGE_PYTHON_DESTINATION = 'lib/python3/dist-packages/genjava'
+CATKIN_PACKAGE_SHARE_DESTINATION = 'share/genjava'
+
+CMAKE_BINARY_DIR = '/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava'
+CMAKE_CURRENT_BINARY_DIR = '/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava'
+CMAKE_CURRENT_SOURCE_DIR = '/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava'
+CMAKE_INSTALL_PREFIX = '/usr/local'
+CMAKE_SOURCE_DIR = '/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava'
+
+PKG_CMAKE_DIR = '/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava/devel/share/genjava/cmake'
+
+PROJECT_NAME = 'genjava'
+PROJECT_BINARY_DIR = '/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava'
+PROJECT_SOURCE_DIR = '/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava'
