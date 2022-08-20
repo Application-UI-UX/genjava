8b78bd9506a3365f7317e0a83c7ce0fe3a48b3b9
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-08-20 18:32:05 -0400

    build

--- catkin_generated/setup_cached.sh
+++ catkin_generated/setup_cached.sh
@@ -7,12 +7,3 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava/devel/lib:$LD_LIBRARY_PATH"
-export PATH='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava/devel/bin:/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/rosjava_build_tools/devel/bin:/opt/ros/noetic/bin:/home/ronaldsonbellande-hplaptop/.local/share/nvim/lsp_servers/cmake/venv/bin:/home/ronaldsonbellande-hplaptop/.local/bin:/opt/gradle/gradle-7.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
-export PKG_CONFIG_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
-export PYTHONPATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava/devel/lib/python3/dist-packages:$PYTHONPATH"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_MAVEN_DEPLOYMENT_REPOSITORY='/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava/devel/share/maven'
-export ROS_MAVEN_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava/devel/share/maven:$ROS_MAVEN_PATH"
-export ROS_PACKAGE_PATH="/home/ronaldsonbellande-hplaptop/Desktop/User_Interface_Experience/genjava:$ROS_PACKAGE_PATH"
